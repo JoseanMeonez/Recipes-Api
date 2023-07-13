ef4e721a8fd282bca1acafc2049a4dbfdfca0367
Josean <joseanmeonez@gmail.com>
2023-07-12 22:03:31 -0600

    Adding Data Models to inputs.

---
+++ Data/PersonInput.cs
@@ -0,0 +1,15 @@
+﻿namespace Recipes_Api.Data
+{
+	public class PersonInput
+	{
+		public int? Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string LastName { get; set; } = null!;
+
+		public int Age { get; set; }
+
+		public string Email { get; set; } = null!;
+	}
+}
---
+++ Data/RecipeInput.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Recipes_Api.Data
+{
+	public class RecipeInput
+	{
+		public int? Id { get; set; }
+
+		[Required]
+		public string Title { get; set; } = null!;
+
+		[Required]
+		public string Description { get; set; } = null!;
+
+		[Required]
+		public DateTime DateCreated { get; set; }
+
+		[Required]
+		public int PersonId { get; set; }
+	}
+}
